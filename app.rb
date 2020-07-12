ca930610c8ed510c1f97705e7b045ca320b397e1
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-12 00:36:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,6 +20,7 @@ class App < Sinatra::Base
   end
   
   get '/multiply' do
-    @product = params[:number1].to_i * params[:number2]
+    @product = params[:number1].to_i * params[:number2].to_i
+    "#{@product}"
   end
 end
